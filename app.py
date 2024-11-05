d49720e59f51d8307df2bb9a26371ea5e9c9c04b
Kirill <187307451+shirmanovka@users.noreply.github.com>
2024-11-05 19:05:35 +0500

    app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ df['Cspread'] = round(df['spread'] + df['Cupon'] + df['Yield'])
 df['deltaS'] = round(df['Cspread'] - df['spread'])
 df['Name_rating_gap'] = df.apply(lambda row: f"{row['Тикер']},{row['Рейтинг']},{row['deltaS']}", axis=1)
 df['Размещениеt'] = pd.to_datetime(df['Размещение'], dayfirst=True)
-df = df.sort_values(by='Размещениеt',ascending=True) #Cортируем от малых к большим
+df = df.sort_values(by='Размещениеt', ascending=True)  # Сортируем от малых к большим
 
 # Создаем Streamlit интерфейс
 st.title('Карта рынка флоутеров')
@@ -28,9 +28,16 @@ selected_tickers = st.multiselect('Выберите тикер:', tickers)
 ratings = df['Рейтинг'].unique()
 selected_ratings = st.multiselect('Выберите рейтинг:', ratings)
 
+# Фильтр по диапазону дат
+min_date = df['Размещениеt'].min()
+max_date = df['Размещениеt'].max()
+selected_date_range = st.date_input("Выберите диапазон дат", [min_date, max_date])
+
 # Фильтрация данных
 f_df = df[(df['Тикер'].isin(selected_tickers) | (len(selected_tickers) == 0)) &
-            (df['Рейтинг'].isin(selected_ratings) | (len(selected_ratings) == 0))]
+            (df['Рейтинг'].isin(selected_ratings) | (len(selected_ratings) == 0)) &
+            (df['Размещениеt'] >= selected_date_range[0]) &
+            (df['Размещениеt'] <= selected_date_range[1])]
 
 # Отображение отфильтрованного DataFrame
 st.dataframe(f_df)
@@ -39,19 +46,18 @@ st.dataframe(f_df)
 if not f_df.empty:
     plt.figure(figsize=(12, 6))
 
-    plt.scatter(f_df['Размещение'], f_df['Cspread'], color='darkred', marker='o', s=80, label='Текущий спред')
-    plt.scatter(f_df['Размещение'], f_df['spread'], color='tan', marker='o', s=80, label='Спред при размещении')
+    plt.scatter(f_df['Размещениеt'], f_df['Cspread'], color='darkred', marker='o', s=80, label='Текущий спред')
+    plt.scatter(f_df['Размещениеt'], f_df['spread'], color='tan', marker='o', s=80, label='Спред при размещении')
 
     for i, row in f_df.iterrows():
-        plt.text(row['Размещение'], row['spread'] + 4, row['Name_rating_gap'], ha='left', fontsize=10)
+        plt.text(row['Размещениеt'], row['spread'] + 4, row['Name_rating_gap'], ha='left', fontsize=10)
         
     for i in range(len(f_df)):
         for j in range(len(f_df)): 
-                    if f_df['Размещение'].iloc[i] == f_df['Размещение'].iloc[j]:
-                    
-                            plt.annotate ('', xy = (f_df['Размещение'].iloc[j], f_df['Cspread'].iloc[j]),
-                                            xytext=(f_df['Размещение'].iloc[i], f_df['spread'].iloc[i]),
-                                            arrowprops =dict(arrowstyle='->', color='goldenrod', linewidth=2, shrinkA=7,shrinkB=7)) #Рисуем стрелки над точками.    
+            if f_df['Размещениеt'].iloc[i] == f_df['Размещениеt'].iloc[j]:
+                plt.annotate('', xy=(f_df['Размещениеt'].iloc[j], f_df['Cspread'].iloc[j]),
+                             xytext=(f_df['Размещениеt'].iloc[i], f_df['spread'].iloc[i]),
+                             arrowprops=dict(arrowstyle='->', color='goldenrod', linewidth=2, shrinkA=7, shrinkB=7))  # Рисуем стрелки над точками.    
 
     plt.title('Карта рынка', fontsize=18)
     plt.xlabel('Дата размещения', fontsize=16)
