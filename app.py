620339582134c04c77e090a4391e96898d9aeb04
Kirill <187307451+shirmanovka@users.noreply.github.com>
2024-11-17 12:54:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,23 @@ import pandas as pd
 import json
 import streamlit as st
 
-# Получаем данные о бондах с MOEX
+# Часть_1 формируем колонки для дата фрейма
 moex_url = 'https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQCB/securities.json' 
-response = requests.get(moex_url) 
+response = requests.get(moex_url) #получим овтет от сервера 
 result = json.loads(response.text)
 col_name1 = result['marketdata']['columns']
+data_bonds_marketdata = pd.DataFrame(columns = col_name1)
 
-# Заполняем DataFrame с данными о бондах
+# Часть_2 заполняем дата фрейм
+moex_url_marketdata = 'https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQCB/securities.json' #TQOB ОФЗ
+response = requests.get(moex_url_marketdata)
+result = json.loads(response.text)
 resp_date = result['marketdata']['data']
-market_data_bonds = pd.DataFrame(resp_date, columns=col_name1)
+market_data_bonds = pd.DataFrame(resp_date, columns = col_name1)
+a = len(resp_date)
 
 # Создаем интерфейс Streamlit
 st.title('Анализ бондов')
-
-# Фильтры для столбцов
-for col in col_name1:
-    if col != 'TRADESTATUS':  # Исключаем столбцы, которые не являются фильтрами
-        unique_values = market_data_bonds[col].unique()
-        selected_values = st.multiselect(f'Выберите значение для {col}:', unique_values, default=unique_values.tolist())
-        # Фильтруем данные
-        market_data_bonds = market_data_bonds[market_data_bonds[col].isin(selected_values)]
 
 # Отображаем отфильтрованный DataFrame
 st.dataframe(market_data_bonds)
