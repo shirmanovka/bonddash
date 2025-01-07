dee5dad5598c87468627a5712e8dd7c91ed70c6e
Kirill <187307451+shirmanovka@users.noreply.github.com>
2025-01-07 17:16:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,40 @@
 import streamlit as st
 import requests
 import pandas as pd
-import json
+import altair as alt
 
 # Заголовок приложения
-st.title("Курс рубля, ЦБ РФ")
+st.title("График кривых свопов")
 
 # Функция для получения данных
-def get_exchange_rates():
-    moex_url_cbrf = 'https://iss.moex.com//iss/statistics/engines/currency/markets/selt/rates.json'
+def get_swap_curves():
+    moex_url = 'https://iss.moex.com//iss/sdfi/curves/securities.json'
     
     try:
-        response = requests.get(moex_url_cbrf)
+        response = requests.get(moex_url)
         if response.status_code == 200:
             result = response.json()
-            col_names = result['cbrf']['columns']
-            df = pd.DataFrame(result['cbrf']['data'], columns=col_names)
-            
-            selected_columns = [
-                'CBRF_USD_LAST',
-                'CBRF_USD_LASTCHANGEPRCNT',
-                'CBRF_USD_TRADEDATE',
-                'CBRF_EUR_LAST',
-                'CBRF_EUR_LASTCHANGEPRCNT',
-                'CBRF_EUR_TRADEDATE'
-            ]
-            filtered_df = df[selected_columns]
-            return filtered_df
+            col_names = result['curves']['columns']
+            df = pd.DataFrame(result['curves']['data'], columns=col_names)
+            return df
         else:
             st.error(f'Ошибка при получении данных. Код состояния: {response.status_code}')
     except Exception as e:
         st.error(f'Произошла ошибка при запросе данных: {e}')
 
 # Отображение данных
-if st.button('Обновить курс'):
-    exchange_rates = get_exchange_rates()
-    if exchange_rates is not None:
-        # Преобразуем DataFrame в нужную форму
-        new_index = ['Доллар', 'Евро']
-        new_columns = ['Курс', 'Изменение', 'Дата обновления']
-        new_values = {
-            'Курс': [exchange_rates['CBRF_USD_LAST'].values[0], exchange_rates['CBRF_EUR_LAST'].values[0]],
-            'Изменение': [exchange_rates['CBRF_USD_LASTCHANGEPRCNT'].values[0], exchange_rates['CBRF_EUR_LASTCHANGEPRCNT'].values[0]],
-            'Дата обновления': [pd.to_datetime(exchange_rates['CBRF_USD_TRADEDATE']).dt.date.values[0],
-                                pd.to_datetime(exchange_rates['CBRF_EUR_TRADEDATE']).dt.date.values[0]]
-        }
-        new_df = pd.DataFrame(new_values, index=new_index, columns=new_columns)
+if st.button('Загрузить данные'):
+    curves_data = get_swap_curves()
+    if curves_data is not None:
+        # Фильтруем данные по swap_curve
+        swap_curve_filter = st.selectbox('Выберите кривую:', options=curves_data['curveid'].unique())
+        filtered_data = curves_data.query("curveid == @swap_curve_filter")
         
-        # Стилизация ячейки с изменением
-        def style_change(value):
-            color = 'green' if value >= 0 else 'red'
-            return f'color: {color}'
+        # Строим график
+        chart = alt.Chart(filtered_data).mark_line(color='red').encode(
+            x='tenor:T',
+            y='swaprate:Q'
+        ).properties(title=f"Кривая '{swap_curve_filter}'")
         
-        # Применяем стилизацию к столбцу "Изменение"
-        styled_df = new_df.style.applymap(style_change, subset=['Изменение'])
-        
-        # Отображаем новую таблицу
-        st.dataframe(styled_df)
+        # Отображаем график
+        st.altair_chart(chart, use_container_width=True)
