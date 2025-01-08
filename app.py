289e3494e9c0961dc124237bc2ed09e02e76e170
Kirill <187307451+shirmanovka@users.noreply.github.com>
2025-01-08 14:27:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,21 +37,13 @@ def get_exchange_rates():
             result = response.json()
             col_names = result['cbrf']['columns']
             df = pd.DataFrame(result['cbrf']['data'], columns=col_names)
-            
-            selected_columns = [
-                'CBRF_USD_LAST',
-                'CBRF_USD_LASTCHANGEPRCNT',
-                'CBRF_USD_TRADEDATE',
-                'CBRF_EUR_LAST',
-                'CBRF_EUR_LASTCHANGEPRCNT',
-                'CBRF_EUR_TRADEDATE'
-            ]
-            filtered_df = df[selected_columns]
-            return filtered_df
+            return df
         else:
             st.error(f'Ошибка при получении данных. Код состояния: {response.status_code}')
     except Exception as e:
         st.error(f'Произошла ошибка при запросе данных: {e}')
+
+
 def get_swap_curves():
     moex_url = 'https://iss.moex.com//iss/sdfi/curves/securities.json'
     
