e2751fe74116a6719da841b1e449fb82e2f9a89e
Kirill <187307451+shirmanovka@users.noreply.github.com>
2025-01-08 13:49:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import pandas as pd
 import plotly.express as px
 from datetime import datetime
 
+
 # Заголовок приложения
 st.title("Финансовые инструменты")
+
 
 # Функция для получения данных ставки ЦБ РФ
 def get_exchange_rates():
@@ -33,6 +35,7 @@ def get_exchange_rates():
     except Exception as e:
         st.error(f'Произошла ошибка при запросе данных: {e}')
 
+
 # Функция для получения данных кривых свопов
 def get_swap_curves():
     moex_url = 'https://iss.moex.com//iss/sdfi/curves/securities.json'
@@ -48,6 +51,30 @@ def get_swap_curves():
             st.error(f'Ошибка при получении данных. Код состояния: {response.status_code}')
     except Exception as e:
         st.error(f'Произошла ошибка при запросе данных: {e}')
+
+
+# Функция для получения общих данных с Мосбиржи
+def get_data(url):
+    response = requests.get(url)
+    result = response.json()
+    col_names = result['marketdata']['columns']
+    data = pd.DataFrame(result['marketdata']['data'], columns=col_names)
+    return data
+
+
+# Функция для загрузки индекса RGBI
+def load_rgbi():
+    moex_url = 'https://iss.moex.com/iss/engines/stock/markets/index/securities/RGBI.json'
+    df = get_data(moex_url)
+    return df
+
+
+# Функция для загрузки индекса IMOEX
+def load_imoex():
+    moex_url = 'https://iss.moex.com/iss/engines/stock/markets/index/securities/IMOEX.json'
+    df = get_data(moex_url)
+    return df
+
 
 # Блок с данными ставки ЦБ РФ
 st.header("Курс рубля, ЦБ РФ")
@@ -81,6 +108,7 @@ if exchange_rates is not None:
         st.markdown(f"Изменение: <span style='color:{change_color}; font-weight:bold; font-size:16px;'>+{abs(eur_change):.2f}%</span>", unsafe_allow_html=True)
         st.write(f"Дата обновления: {eur_trade_date}")
 
+
 # Блок с графиками кривых свопов
 st.header("Графики кривых свопов")
 
@@ -101,14 +129,33 @@ if curves_data is not None:
         st.write(f"Дата выгрузки: {trade_date}")
         
         # Строим график
-        fig = px.line(filtered_data, x='tenor', y='swap_rate', title=f"Кривая '{swap_curve_filter}'",
-                     labels={'tenor': 'Срок', 'swap_rate': 'Ставка'},
-                     template='plotly_dark')
-        
-        # Отображаем график
+        fig = px.line(filtered_data, x='tenor', y='swap_rate', title=f'Кривая свопа "{swap_curve_filter}"')
         st.plotly_chart(fig, use_container_width=True)
-    else:
-        st.warning("Столбец 'swap_curve' отсутствует в данных.")
-else:
-    st.warning("Не удалось получить данные.")
 
+
+# Индексы RGBI и IMOEX
+st.header("Индексы")
+left_column, right_column = st.columns(2)
+
+with left_column:
+    st.subheader(f"RGBI: {load_rgbi()['CURRENTVALUE'].values[0]}")
+    
+    rgbi_df = load_rgbi()
+    
+    last_change = float(rgbi_df['LASTCHANGEPRC'].values[0])
+    change_color = "green" if last_change >= 0 else "red"
+    st.markdown(f"Изменение к закрытию: <span style='color:{change_color}; font-weight:bold; font-size:16px;'>{last_change:.2f}%</span>", unsafe_allow_html=True)
+    st.text(f"Дата обновления: {rgbi_df['SYSTIME'].values[0]}")
+
+with right_column:
+    st.subheader(f"IMOEX: {load_imoex()['CURRENTVALUE'].values[0]}")
+    
+    imoex_df = load_imoex()
+    
+    last_change = float(imoex_df['LASTCHANGEPRC'].values[0])
+    change_color = "green" if last_change >= 0 else "red"
+    st.markdown(f"Изменение к закрытию: <span style='color:{change_color}; font-weight:bold; font-size:16px;'>{last_change:.2f}%</span>", unsafe_allow_html=True)
+    st.text(f"Дата обновления: {imoex_df['SYSTIME'].values[0]}")
+
+if st.button('Обновить данные', key='refresh'):
+    st.experimental_rerun()
